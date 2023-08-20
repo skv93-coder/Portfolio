96860668a28bc9f7e4ce35dcef09a13ce448f893
Shubham Sharma <shbh541@gmail.com>
2023-08-20 18:30:03 +0530

    Add portfolio link

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Typewriter from "typewriter-effect";
 
 import styles from "../styles/Home.module.css";
-import Icons, { links } from "../Components/svg";
+import Icons, { links, techLogo } from "../Components/svg";
 import Image from "next/image";
 
 export default function Home() {
@@ -149,12 +149,14 @@ export default function Home() {
               name: "MATH CLASH",
               github: "Math-clash",
               link: "https://skv93-coder.github.io/Math-clash/",
+              tech: ["js"],
             },
             {
               img: "/portfolio-1.png",
               name: "Portfolio",
               github: "Portfolio",
-              // link: "",
+              link: "https://portfolio-skv93-coder.vercel.app/",
+              tech: ["vercel", "js"],
             },
           ].map((project) => (
             <div className="project-card">
@@ -163,7 +165,7 @@ export default function Home() {
                 <h2>{project.name} </h2>
 
                 <div className="icons">
-                  <div className="icon cursor-pointer">
+                  <div className="icon icon_hover cursor-pointer">
                     <a
                       href={`https://github.com/skv93-coder/${project.github}`}
                       target="_blank"
@@ -178,31 +180,12 @@ export default function Home() {
                       </a>
                     </div>
                   )}
-                  <div className="icon">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 128 128"
-                    >
-                      <path
-                        fill="#F0DB4F"
-                        d="M1.408 1.408h125.184v125.185h-125.184z"
-                      />
-                      <path
-                        fill="#323330"
-                        d="M116.347 96.736c-.917-5.711-4.641-10.508-15.672-14.981-3.832-1.761-8.104-3.022-9.377-5.926-.452-1.69-.512-2.642-.226-3.665.821-3.32 4.784-4.355 7.925-3.403 2.023.678 3.938 2.237 5.093 4.724 5.402-3.498 5.391-3.475 9.163-5.879-1.381-2.141-2.118-3.129-3.022-4.045-3.249-3.629-7.676-5.498-14.756-5.355l-3.688.477c-3.534.893-6.902 2.748-8.877 5.235-5.926 6.724-4.236 18.492 2.975 23.335 7.104 5.332 17.54 6.545 18.873 11.531 1.297 6.104-4.486 8.08-10.234 7.378-4.236-.881-6.592-3.034-9.139-6.949-4.688 2.713-4.688 2.713-9.508 5.485 1.143 2.499 2.344 3.63 4.26 5.795 9.068 9.198 31.76 8.746 35.83-5.176.165-.478 1.261-3.666.38-8.581zm-46.885-37.793h-11.709l-.048 30.272c0 6.438.333 12.34-.714 14.149-1.713 3.558-6.152 3.117-8.175 2.427-2.059-1.012-3.106-2.451-4.319-4.485-.333-.584-.583-1.036-.667-1.071l-9.52 5.83c1.583 3.249 3.915 6.069 6.902 7.901 4.462 2.678 10.459 3.499 16.731 2.059 4.082-1.189 7.604-3.652 9.448-7.401 2.666-4.915 2.094-10.864 2.07-17.444.06-10.735.001-21.468.001-32.237z"
-                      />
-                      <div
-                        xmlns=""
-                        id="divScriptsUsed"
-                        style={{ display: "none" }}
-                      />
-                      <script
-                        xmlns=""
-                        id="globalVarsDetection"
-                        src="chrome-extension://cmkdbmfndkfgebldhnkbfhlneefdaaip/js/wrs_env.js"
-                      />
-                    </svg>
-                  </div>
+
+                  {project.tech.map((r) => (
+                    <div className="icon" title={r}>
+                      {techLogo[r]}
+                    </div>
+                  ))}
                 </div>
               </div>
             </div>
